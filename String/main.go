62e1c566c15a7a2f3e13975d4425226535838c15
Qianqian Zhou <qqzh1214@gmail.com>
2025-09-17 22:19:16 +0200

    string example

---
+++ String/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
+// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
+
+func main() {
+
+	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
+	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
+	if len(os.Args) < 3 {
+		fmt.Fprintln(os.Stderr, "no enough args")
+		os.Exit(-1)
+	}
+
+	old, new := os.Args[1], os.Args[2]
+	scan := bufio.NewScanner(os.Stdin)
+
+	for scan.Scan() {
+		s := strings.Split(scan.Text(), old)
+		t := strings.Join(s, new)
+
+		fmt.Println(t)
+	}
+}
