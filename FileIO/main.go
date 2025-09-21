d5f2a5f8c438492f6025f517fcc5fd935d486b92
Qianqian Zhou <qqzh1214@gmail.com>
2025-09-21 15:50:56 +0200

    buifo

--- FileIO/main.go
+++ FileIO/main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io/ioutil"
 	"os"
+	"strings"
 )
 
 func main() {
 	for _, fname := range os.Args[1:] { // loop all file names in cmdLine
+		var lc, wc, cc int
 		file, err := os.Open(fname)
 
 		if err != nil {
@@ -15,14 +17,21 @@ func main() {
 			continue
 		}
 
-		data, err := ioutil.ReadAll(file)
-
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			continue
 		}
 
-		fmt.Println("The file has", len(data), "bytes")
+		scan := bufio.NewScanner(file)
+
+		for scan.Scan() {
+			s := scan.Text()
+			wc += len(strings.Fields(s)) //Filds is a func takes string and return a slice of string
+			cc += len(s)
+			lc++
+		}
+
+		fmt.Printf("%7d %7d %7d %s\n", lc, wc, cc, fname)
 		file.Close()
 	}
 }
