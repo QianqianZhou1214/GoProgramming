7fc7055e47cf6d2176dee2b19a3f53472982ae5c
Qianqian Zhou <qqzh1214@gmail.com>
2025-09-22 20:28:29 +0200

    Param Map pointer

--- Functions/main.go
+++ Functions/main.go
@@ -4,16 +4,16 @@ import (
 	"fmt"
 )
 
-// map is reference type
-func do(m1 map[int]int) {
-	m1[3] = 0
-	m1 = make(map[int]int) //reassigned to a new empty map
-	m1[4] = 4              //local variable
-	fmt.Println("m1", m1)
+func do(m1 *map[int]int) { // pointer
+	(*m1)[3] = 0
+	*m1 = make(map[int]int) // if want to change the reference of map(point to new map) then use pointer
+	(*m1)[4] = 4
+	fmt.Println("m1", *m1)
 }
 func main() {
 	m := map[int]int{4: 1, 7: 2, 8: 3}
-	do(m)
+	fmt.Println("m", m)
+	do(&m)
 
 	fmt.Println("m", m)
 }
