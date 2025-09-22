2c7f45285d1d06196e3a84071a0ef6c24edefdc1
Qianqian Zhou <qqzh1214@gmail.com>
2025-09-22 19:57:31 +0200

    Check pointers of slice

--- Functions/main.go
+++ Functions/main.go
@@ -6,10 +6,12 @@ import (
 
 func do(b []int) int {
 	b[0] = 0
+	fmt.Printf("b@ %p\n", b)
 	return b[1]
 }
 func main() {
 	a := []int{1, 2, 3} //slice
+	fmt.Printf("a@ %p\n", a)
 	v := do(a)
 
 	fmt.Println(a, v)
