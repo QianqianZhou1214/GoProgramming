a33c3cd882f700ad804f8d7df1f2f118ae164455
Qianqian Zhou <qqzh1214@gmail.com>
2025-09-28 17:03:37 +0200

    refactoring: comments func public/private

--- LearnGoByTest/HelloWorld/main.go
+++ LearnGoByTest/HelloWorld/main.go
@@ -4,28 +4,34 @@ import (
 	"fmt"
 )
 
-const french = "French"
-const spanish = "Spanish"
-const englishHelloPrefix = "Hello, "
-const spanishHelloPrefix = "Hola, "
-const frenchHelloPrefix = "Bonjour, "
+const (
+	spanish = "Spanish"
+	french  = "French"
 
-func Hello(name string, language string) string {
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+func Hello(name string, language string) string { //public func starts with uppercase letter
 	if name == "" {
 		name = "World"
 	}
 
-	prefix := englishHelloPrefix
+	return greetingPrefix(language) + name
+}
 
+func greetingPrefix(language string) (prefix string) {
 	switch language {
 	case french:
 		prefix = frenchHelloPrefix
 	case spanish:
 		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 
 	}
-
-	return prefix + name
+	return
 }
 
 func main() {
