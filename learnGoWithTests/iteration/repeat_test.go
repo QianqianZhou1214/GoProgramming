5e37d5a8a6a57bf68cecceb0c641ef8946c77de6
Qianqian Zhou <qqzh1214@gmail.com>
2025-09-29 22:13:02 +0200

    ExampleRepeat

--- learnGoWithTests/iteration/repeat_test.go
+++ learnGoWithTests/iteration/repeat_test.go
@@ -1,9 +1,12 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
+	repeated := Repeat("a", 5)
 	expected := "aaaaa"
 
 	if repeated != expected {
@@ -14,7 +17,13 @@ func TestRepeat(t *testing.T) {
 func BenchmarkRepeat(b *testing.B) {
 	// setup
 	for b.Loop() {
-		Repeat("a")
+		Repeat("a", 5)
 	}
 	// cleanup
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("a", 5)
+	fmt.Println(repeated)
+	// Output: aaaaa
 }
