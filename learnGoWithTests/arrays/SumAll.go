61117cc034b1bcf8e18114dd5c1231f1b88b1359
Qianqian Zhou <qqzh1214@gmail.com>
2025-10-01 20:52:52 +0200

    SumAll implementation

--- learnGoWithTests/arrays/SumAll.go
+++ learnGoWithTests/arrays/SumAll.go
@@ -1,7 +1,14 @@
 package arrays
 
 func SumAll(numbersToSum ...[]int) []int { // this is a Variadic function
-	return nil
+	// the type of numbersToSum is equivalent to []int,
+	//which means we can use len() and iterates over it with range.
+	lengthOfNumbers := len(numbersToSum)
+	sums := make([]int, lengthOfNumbers)
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+	return sums
 }
 
 // fmt.Println() is a common variadic function (with any number of trailing arguments)
