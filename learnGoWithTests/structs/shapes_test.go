7fb11c47562b88dd3d5e68e107ca327132852c28
Qianqian Zhou <qqzh1214@gmail.com>
2025-10-03 15:12:32 +0200

    Refactoring the test

--- learnGoWithTests/structs/shapes_test.go
+++ learnGoWithTests/structs/shapes_test.go
@@ -17,7 +17,7 @@ func TestPerimeter(t *testing.T) {
 func TestArea(t *testing.T) {
 	t.Run("rectangle", func(t *testing.T) {
 		rectangle := Rectangle{12, 6}
-		got := Area(rectangle)
+		got := rectangle.Area()
 		want := 72.0
 		if got != want {
 			t.Errorf("got %.2f want %.2f", got, want)
@@ -26,7 +26,7 @@ func TestArea(t *testing.T) {
 
 	t.Run("circle", func(t *testing.T) {
 		circle := Circle{10}
-		got := Area(circle)
+		got := circle.Area()
 		want := 314.1592653589793
 		if got != want {
 			t.Errorf("got %g want %g", got, want) //g: more precise
