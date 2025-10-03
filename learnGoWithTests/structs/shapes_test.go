dfbd2018280b99421398b0136450bbc55f33d711
Qianqian Zhou <qqzh1214@gmail.com>
2025-10-03 14:45:34 +0200

    Write test for Circle Area

--- learnGoWithTests/structs/shapes_test.go
+++ learnGoWithTests/structs/shapes_test.go
@@ -15,10 +15,22 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{12, 6}
-	got := Area(rectangle)
-	want := 72.0
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
+	t.Run("rectangle", func(t *testing.T) {
+		rectangle := Rectangle{12, 6}
+		got := Area(rectangle)
+		want := 72.0
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+
+	t.Run("circle", func(t *testing.T) {
+		circle := Circle{10}
+		got := Area(circle)
+		want := 314.1592653589793
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+
 }
