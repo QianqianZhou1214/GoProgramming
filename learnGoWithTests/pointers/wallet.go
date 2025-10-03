5ece937bf48ccdef60cb7728a9636aca9e2fc129
Qianqian Zhou <qqzh1214@gmail.com>
2025-10-03 19:40:36 +0200

    Refactoring to Bitcoin

--- learnGoWithTests/pointers/wallet.go
+++ learnGoWithTests/pointers/wallet.go
@@ -2,16 +2,17 @@ package pointers
 
 import "fmt"
 
+type Bitcoin int
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	fmt.Printf("address of balance in test is %p \n", &w.balance)
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance // can be (*w).balance, but Go permits us to write w.balance.
 	// It's called struct pointer.
 }
