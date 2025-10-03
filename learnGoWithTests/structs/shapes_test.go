613c7d78dc2ce842eb6cbe41199435177fbe1783
Qianqian Zhou <qqzh1214@gmail.com>
2025-10-03 14:22:15 +0200

    wrong numbers

--- learnGoWithTests/structs/shapes_test.go
+++ learnGoWithTests/structs/shapes_test.go
@@ -14,7 +14,7 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	got := Area(10.0, 10.0)
+	got := Area(12.0, 6.0)
 	want := 72.0
 	if got != want {
 		t.Errorf("got %.2f want %.2f", got, want)
