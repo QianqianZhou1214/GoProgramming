ece65866460264c002203653509a996549a2f2cf
Qianqian Zhou <qqzh1214@gmail.com>
2025-10-03 19:31:29 +0200

    check the address

--- learnGoWithTests/pointers/wallet.go
+++ learnGoWithTests/pointers/wallet.go
@@ -1,10 +1,13 @@
 package pointers
 
+import "fmt"
+
 type Wallet struct {
 	balance int
 }
 
 func (w Wallet) Deposit(amount int) {
+	fmt.Printf("address of balance in test is %p \n", &w.balance)
 	w.balance += amount
 }
 
--- learnGoWithTests/pointers/wallet_test.go
+++ learnGoWithTests/pointers/wallet_test.go
@@ -1,12 +1,17 @@
 package pointers
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestWallet(t *testing.T) {
 	wallet := Wallet{}
 	wallet.Deposit(10)
 
 	got := wallet.Balance()
+
+	fmt.Printf("address of balance in test is %p \n", &wallet.balance)
 	want := 10
 
 	if got != want {
