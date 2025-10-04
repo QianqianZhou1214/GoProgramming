eb87718d459463b5b113583b225d963817ce0e79
Qianqian Zhou <qqzh1214@gmail.com>
2025-10-04 21:06:12 +0200

    Refactor: use custom type

--- learnGoWithTests/maps/dictionary_test.go
+++ learnGoWithTests/maps/dictionary_test.go
@@ -6,7 +6,7 @@ func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 	// key type can only be comparable type; value type can be any even a map
 
-	got := dictionary.Search(dictionary, "test")
+	got := dictionary.Search("test")
 	want := "this is just a test"
 	assertStrings(t, got, want)
 }
