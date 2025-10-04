ab744c04a487360bc44a351cbba3b7616948a640
Qianqian Zhou <qqzh1214@gmail.com>
2025-10-04 20:49:01 +0200

    Test comments and fixed

--- learnGoWithTests/maps/dictionary_test.go
+++ learnGoWithTests/maps/dictionary_test.go
@@ -4,8 +4,9 @@ import "testing"
 
 func TestSearch(t *testing.T) {
 	dictionary := map[string]string{"test": "this is just a test"}
+	// key type can only be comparable type; value type can be any even a map
 
-	got := dictionary["test"]
+	got := Search(dictionary, "test")
 	want := "this is just a test"
 	if got != want {
 		t.Errorf("got %q, want %q, given %q", got, want, "test")
