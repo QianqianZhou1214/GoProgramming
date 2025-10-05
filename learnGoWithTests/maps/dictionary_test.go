b10fdd94630c47f7b49151185c32637172be8318
Qianqian Zhou <qqzh1214@gmail.com>
2025-10-05 14:08:29 +0200

    Implemented Add func

--- learnGoWithTests/maps/dictionary_test.go
+++ learnGoWithTests/maps/dictionary_test.go
@@ -2,6 +2,20 @@ package maps
 
 import "testing"
 
+func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func assertError(t *testing.T, got, want error) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got error %q, want %q", got, want)
+	}
+}
+
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 	// key type can only be comparable type; value type can be any even a map
@@ -24,16 +38,14 @@ func TestSearch(t *testing.T) {
 
 }
 
-func assertStrings(t testing.TB, got, want string) {
-	t.Helper()
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
-}
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	dictionary.Add("test", "this is just a test")
 
-func assertError(t *testing.T, got, want error) {
-	t.Helper()
-	if got != want {
-		t.Errorf("got error %q, want %q", got, want)
+	want := "this is just a test"
+	got, err := dictionary.Search("test")
+	if err != nil {
+		t.Fatal("should find added word: ", err)
 	}
+	assertStrings(t, got, want)
 }
