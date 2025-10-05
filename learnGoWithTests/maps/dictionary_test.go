228ab3b87980529ee6a7b1fd6b371e64b8cdb748
Qianqian Zhou <qqzh1214@gmail.com>
2025-10-05 13:54:22 +0200

    implemented unknown word error

--- learnGoWithTests/maps/dictionary_test.go
+++ learnGoWithTests/maps/dictionary_test.go
@@ -6,9 +6,23 @@ func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 	// key type can only be comparable type; value type can be any even a map
 
-	got := dictionary.Search("test")
-	want := "this is just a test"
-	assertStrings(t, got, want)
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		want := "could not find the word you were looking for"
+		if err == nil {
+			t.Fatal("expected to get an error")
+		}
+
+		assertStrings(t, err.Error(), want)
+		// error is not nil, use .Error() to get string which we can pass to asset()
+	})
+
 }
 
 func assertStrings(t testing.TB, got, want string) {
