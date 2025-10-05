786bfdea07ff28637187615ef0ce99de3d66ba6b
Qianqian Zhou <qqzh1214@gmail.com>
2025-10-05 14:41:29 +0200

    testAdd for existing word

--- learnGoWithTests/maps/dictionary_test.go
+++ learnGoWithTests/maps/dictionary_test.go
@@ -38,16 +38,6 @@ func TestSearch(t *testing.T) {
 
 }
 
-func TestAdd(t *testing.T) {
-	dictionary := Dictionary{}
-	word := "test"
-	definition := "this is just a test"
-
-	dictionary.Add(word, definition)
-
-	assertDefinition(t, dictionary, word, definition)
-}
-
 func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
 	t.Helper()
 	got, err := dictionary.Search(word)
@@ -55,4 +45,27 @@ func assertDefinition(t testing.TB, dictionary Dictionary, word, definition stri
 		t.Fatal("should find added word: ", err)
 	}
 	assertStrings(t, got, definition)
+}
+
+func TestAdd(t *testing.T) {
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		word := "test"
+		definition := "this is just a test"
+
+		err := dictionary.Add(word, definition)
+
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, definition)
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		err := dictionary.Add(word, "new test")
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, definition)
+	})
+
 }
