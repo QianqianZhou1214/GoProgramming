f4799aed63b5cd16c71bff9dc08b7e0b5db31190
Qianqian Zhou <qqzh1214@gmail.com>
2025-10-05 14:36:00 +0200

    Nil map

--- learnGoWithTests/maps/dictionary.go
+++ learnGoWithTests/maps/dictionary.go
@@ -4,6 +4,9 @@ import "errors"
 
 type Dictionary map[string]string
 
+// do not do var m map[string]string to initialize a nil map variable
+// do make map: var dictionary = map[string]string{} OR var dictionary = make(map[string]string)
+
 var ErrNotFound = errors.New("could not find the word you were looking for")
 
 func (d Dictionary) Search(word string) (string, error) {
@@ -16,4 +19,5 @@ func (d Dictionary) Search(word string) (string, error) {
 
 func (d Dictionary) Add(word, definition string) {
 	d[word] = definition
+	// A map value is a pointer to a runtime.hmap structure.
 }
