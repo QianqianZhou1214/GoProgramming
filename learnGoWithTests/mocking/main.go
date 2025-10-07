01ce59a8d15a294a5212e0b453c10127661cf2e7
Qianqian Zhou <qqzh1214@gmail.com>
2025-10-07 20:58:30 +0200

    Refactor

--- learnGoWithTests/mocking/main.go
+++ learnGoWithTests/mocking/main.go
@@ -6,13 +6,16 @@ import (
 	"os"
 )
 
+const finalWord = "Go!"
+const countdownStart = 3
+
 func main() {
 	Countdown(os.Stdout)
 }
 
 func Countdown(out io.Writer) {
-	for i := 3; i > 0; i-- {
+	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
 	}
-	fmt.Fprintf(out, "Go!")
+	fmt.Fprintf(out, finalWord)
 }
