c0cdf140d6a11c4d0d49f65b960db7d9b16767de
Qianqian Zhou <qqzh1214@gmail.com>
2025-10-07 21:08:49 +0200

    Mocking time.Sleep()

--- learnGoWithTests/mocking/main.go
+++ learnGoWithTests/mocking/main.go
@@ -7,14 +7,39 @@ import (
 	"time"
 )
 
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+//for main
+
 const finalWord = "Go!"
 const countdownStart = 3
 
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
 }
 
-func Countdown(out io.Writer) {
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+//for test
+
+//Spies are a kind of mock which can record how a dependency is used.
+
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
 		time.Sleep(1 * time.Second)
