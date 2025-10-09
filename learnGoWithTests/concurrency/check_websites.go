16afff8fc56271a266f3d0756af550e2f5d14122
Qianqian Zhou <qqzh1214@gmail.com>
2025-10-09 21:44:06 +0200

    Implemented goroutine

--- learnGoWithTests/concurrency/check_websites.go
+++ learnGoWithTests/concurrency/check_websites.go
@@ -1,5 +1,7 @@
 package concurrency
 
+import "time"
+
 type WebsiteChecker func(string) bool
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
@@ -11,5 +13,6 @@ func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 		}()
 		// goroutine: often anonymous func
 	}
+	time.Sleep(2 * time.Second)
 	return results
 }
