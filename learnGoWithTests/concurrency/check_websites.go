649816b3c63e01937c1b12cd9d88ea429922056d
Qianqian Zhou <qqzh1214@gmail.com>
2025-10-12 21:15:25 +0200

    Implemented channels

--- learnGoWithTests/concurrency/check_websites.go
+++ learnGoWithTests/concurrency/check_websites.go
@@ -1,18 +1,29 @@
 package concurrency
 
-import "time"
-
 type WebsiteChecker func(string) bool
+
+type result struct {
+	string
+	bool
+}
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultChannel := make(chan result)
+	// channels are a Go data structure that can both receive and send values.
 
 	for _, url := range urls {
 		go func() {
-			results[url] = wc(url)
+			resultChannel <- result{url, wc(url)}
+			//results[url] = wc(url)
 		}()
 		// goroutine: often anonymous func
 	}
-	time.Sleep(2 * time.Second)
+
+	for i := 0; i < len(urls); i++ {
+		r := <-resultChannel
+		results[r.string] = r.bool
+	}
+	//time.Sleep(2 * time.Second)
 	return results
 }
