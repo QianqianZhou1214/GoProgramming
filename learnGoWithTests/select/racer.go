277f06953a48743b92002eaa9a19faada04696b1
Qianqian Zhou <qqzh1214@gmail.com>
2025-10-14 21:41:38 +0200

    Test: two cases

--- learnGoWithTests/select/racer.go
+++ learnGoWithTests/select/racer.go
@@ -15,7 +15,7 @@ func Racer(a, b string) (winner string) {
 }
 
 func ping(url string) chan struct{} {
-	ch := make(chan struct{})
+	ch := make(chan struct{}) // always make channels, do not do var ch because it's nil and cannot assign value by <-.
 	go func() {
 		http.Get(url)
 		close(ch)
