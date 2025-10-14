d6f1ed93ec1edf3e2dfbded526d49262d176daef
Qianqian Zhou <qqzh1214@gmail.com>
2025-10-14 21:46:24 +0200

    refactoring

--- learnGoWithTests/select/racer.go
+++ learnGoWithTests/select/racer.go
@@ -6,16 +6,21 @@ import (
 	"time"
 )
 
+var tenSecondTimeout = 10 * time.Second
+
 func Racer(a, b string) (winner string, error error) {
+	return ConfigurableRacer(a, b, tenSecondTimeout)
+}
+
+func ConfigurableRacer(a, b string, timeOut time.Duration) (winner string, error error) {
 	select {
 	case <-ping(a):
 		return a, nil
 	case <-ping(b):
 		return b, nil
-	case <-time.After(10 * time.Second):
+	case <-time.After(timeOut):
 		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
 	}
-
 }
 func ping(url string) chan struct{} {
 	ch := make(chan struct{}) // always make channels, do not do var ch because it's nil and cannot assign value by <-.
