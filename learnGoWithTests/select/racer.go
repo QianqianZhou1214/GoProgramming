40ad7559e2a73b8c2f95446b11d89b9f526a88bb
Qianqian Zhou <qqzh1214@gmail.com>
2025-10-14 21:44:21 +0200

    refactoring: error in Racer()

--- learnGoWithTests/select/racer.go
+++ learnGoWithTests/select/racer.go
@@ -1,7 +1,9 @@
 package _select
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 )
 
 func Racer(a, b string) (winner string, error error) {
@@ -10,10 +12,11 @@ func Racer(a, b string) (winner string, error error) {
 		return a, nil
 	case <-ping(b):
 		return b, nil
+	case <-time.After(10 * time.Second):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
 	}
 
 }
-
 func ping(url string) chan struct{} {
 	ch := make(chan struct{}) // always make channels, do not do var ch because it's nil and cannot assign value by <-.
 	go func() {
