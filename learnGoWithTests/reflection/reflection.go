e1ac6b4a610f429e25ea45359dc3ad67a344d82e
Qianqian Zhou <qqzh1214@gmail.com>
2025-10-17 20:59:36 +0200

    refactor

--- learnGoWithTests/reflection/reflection.go
+++ learnGoWithTests/reflection/reflection.go
@@ -3,7 +3,7 @@ package reflection
 import "reflect"
 
 func walk(x interface{}, fn func(input string)) {
-	val := reflect.ValueOf(x)
+	val := getValue(x)
 	for i := 0; i < val.NumField(); i++ {
 		field := val.Field(i)
 
@@ -17,4 +17,12 @@ func walk(x interface{}, fn func(input string)) {
 	//field := val.Field(0) //use first and only field unsafe because what if there is not fields
 	//fn(field.String())    // assumed the type is string, what if other than string
 	//fn("I still can't believe South Korea beat Germany 2-0 to put them last in their group")
+}
+
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+	if val.Kind() != reflect.Pointer {
+		val = val.Elem()
+	}
+	return val
 }
