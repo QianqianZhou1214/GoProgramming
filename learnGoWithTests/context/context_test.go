4411624a1606881917e57b454554f08500d92fb1
Qianqian Zhou <qqzh1214@gmail.com>
2025-10-18 20:03:46 +0200

    make another test pass

--- learnGoWithTests/context/context_test.go
+++ learnGoWithTests/context/context_test.go
@@ -2,6 +2,7 @@ package context
 
 import (
 	"context"
+	"errors"
 	"log"
 	"net/http"
 	"net/http/httptest"
@@ -46,6 +47,22 @@ func (s *SpyStore) Fetch(ctx context.Context) (string, error) {
 	*/
 }
 
+type SpyResponseWriter struct {
+	written bool
+}
+
+func (s *SpyResponseWriter) Header() http.Header {
+	s.written = true
+	return nil
+}
+func (s *SpyResponseWriter) Write([]byte) (int, error) {
+	s.written = true
+	return 0, errors.New("not implemented")
+}
+func (s *SpyResponseWriter) WriteHeader(statusCode int) {
+	s.written = true
+}
+
 /*
 func (s *SpyStore) Cancel() {
 
@@ -82,10 +99,13 @@ func TestServer(t *testing.T) {
 		time.AfterFunc(5*time.Millisecond, cancel)
 		request = request.WithContext(cancellingCtx)
 
-		response := httptest.NewRecorder()
+		response := &SpyResponseWriter{}
 
 		svr.ServeHTTP(response, request)
 
+		if response.written {
+			t.Error("a response should not have been written")
+		}
 		/*
 			store.assertWasCancelled()
 
