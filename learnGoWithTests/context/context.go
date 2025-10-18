8c1426bc762897e215f76b95de204f9fe2c7beb9
Qianqian Zhou <qqzh1214@gmail.com>
2025-10-18 20:01:10 +0200

    write code to make happy path pass

--- learnGoWithTests/context/context.go
+++ learnGoWithTests/context/context.go
@@ -2,6 +2,7 @@ package context
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 )
 
@@ -11,6 +12,8 @@ type Store interface {
 
 func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		data, _ := store.Fetch(r.Context())
+		fmt.Fprint(w, data)
 		/*
 			ctx := r.Context()
 
