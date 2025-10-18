1d6e85f55df8f5be8761d7d329a04b30c37c160a
Qianqian Zhou <qqzh1214@gmail.com>
2025-10-18 12:59:01 +0200

    extract maps into new test

--- learnGoWithTests/reflection/reflection_test.go
+++ learnGoWithTests/reflection/reflection_test.go
@@ -103,6 +103,21 @@ func TestWalk(t *testing.T) {
 				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
 			}
 		})
+
+		t.Run("with maps", func(t *testing.T) {
+			aMap := map[string]string{
+				"Cow":   "Moo",
+				"Sheep": "Baa",
+			}
+
+			var got []string
+			walk(aMap, func(input string) {
+				got = append(got, input)
+			})
+
+			assertContains(t, got, "Moo")
+			assertContains(t, got, "Baa")
+		})
 	}
 	/*
 		expected := "Chris"
@@ -125,4 +140,18 @@ func TestWalk(t *testing.T) {
 		}
 
 	*/
+}
+
+func assertContains(t testing.TB, haystack []string, needle string) {
+	t.Helper()
+	contains := false
+	for _, x := range haystack {
+		if x == needle {
+			contains = true
+		}
+	}
+
+	if !contains {
+		t.Errorf("expected %v to contain %q but it didn't", haystack, needle)
+	}
 }
