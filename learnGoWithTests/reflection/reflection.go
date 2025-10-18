545bd96daa145520f6317eebbf13b58d6e47eaee
Qianqian Zhou <qqzh1214@gmail.com>
2025-10-18 12:53:36 +0200

    take steps back

--- learnGoWithTests/reflection/reflection.go
+++ learnGoWithTests/reflection/reflection.go
@@ -5,27 +5,50 @@ import "reflect"
 func walk(x interface{}, fn func(input string)) {
 	val := getValue(x)
 
-	numberOfValues := 0
-	var getField func(int) reflect.Value
+	walkValue := func(value reflect.Value) {
+		walk(value.Interface(), fn)
+	}
 
 	switch val.Kind() {
 	case reflect.String:
 		fn(val.String())
 	case reflect.Struct:
-		numberOfValues = val.NumField()
-		getField = val.Field //Assign the val.Field method (function) to getField.
+		for i := 0; i < val.NumField(); i++ {
+			walkValue(val.Field(i))
+		}
 	case reflect.Slice, reflect.Array:
-		numberOfValues = val.Len()
-		getField = val.Index
+		for i := 0; i < val.Len(); i++ {
+			walkValue(val.Index(i))
+		}
 	case reflect.Map:
 		for _, key := range val.MapKeys() {
-			walk(val.MapIndex(key).Interface(), fn)
+			walkValue(val.MapIndex(key))
 		}
 	}
+	/*
+		numberOfValues := 0
+		var getField func(int) reflect.Value
 
-	for i := 0; i < numberOfValues; i++ {
-		walk(getField(i).Interface(), fn)
-	}
+		switch val.Kind() {
+		case reflect.String:
+			fn(val.String())
+		case reflect.Struct:
+			numberOfValues = val.NumField()
+			getField = val.Field //Assign the val.Field method (function) to getField.
+		case reflect.Slice, reflect.Array:
+			numberOfValues = val.Len()
+			getField = val.Index
+		case reflect.Map:
+			for _, key := range val.MapKeys() {
+				walk(val.MapIndex(key).Interface(), fn)
+			}
+		}
+
+		for i := 0; i < numberOfValues; i++ {
+			walk(getField(i).Interface(), fn)
+		}
+
+	*/
 	/*
 		if val.Kind() == reflect.Slice {
 			for i := 0; i < val.Len(); i++ {
