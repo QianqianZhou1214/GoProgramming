bdc9e2e4a8a091459f9de7ab15fb1333db36c92b
Qianqian Zhou <qqzh1214@gmail.com>
2025-10-19 21:25:55 +0200

    final pass

--- learnGoWithTests/roman-numerals/roman_numerals.go
+++ learnGoWithTests/roman-numerals/roman_numerals.go
@@ -4,12 +4,18 @@ import (
 	"strings"
 )
 
-type RomanNumerals struct {
+type RomanNumeral struct {
 	Value  int
 	Symbol string
 }
 
-var allRomanNumerals = []RomanNumerals{
+var allRomanNumerals = []RomanNumeral{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
 	{50, "L"},
 	{40, "XL"},
 	{10, "X"},
