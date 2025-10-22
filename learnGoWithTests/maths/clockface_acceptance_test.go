ee71d5f5fb57e7196b9d62cfb6aaa9e2cd4f66f6
Qianqian Zhou <qqzh1214@gmail.com>
2025-10-22 21:01:09 +0200

    write the test for SVGWriter

--- learnGoWithTests/maths/clockface_acceptance_test.go
+++ learnGoWithTests/maths/clockface_acceptance_test.go
@@ -2,20 +2,38 @@ package clockface_test
 
 import (
 	clockface "learnGoWithTests/maths"
+	"strings"
 	"testing"
 	"time"
 )
 
-func TestSecondHandAtMidnight(t *testing.T) {
+/*
+	func TestSecondHandAtMidnight(t *testing.T) {
+		tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+
+		want := clockface.Point{X: 150, Y: 150 - 90}
+		got := clockface.SecondHand(tm)
+
+		if got != want {
+			t.Errorf("Got %v, wanted %v", got, want)
+		}
+	}
+*/
+func TestSVGWriterAtMidnight(t *testing.T) {
 	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
 
-	want := clockface.Point{X: 150, Y: 150 - 90}
-	got := clockface.SecondHand(tm)
+	var b strings.Builder
+	clockface.SVGWriter(&b, tm)
+	got := b.String()
 
-	if got != want {
-		t.Errorf("Got %v, wanted %v", got, want)
+	want := `<line x1="150" y1="150" x2="150" y2="60"`
+
+	if !strings.Contains(got, want) {
+		t.Errorf("Expected to find the second hand %v, in the SVG output %v", want, got)
 	}
 }
+
+/*
 
 func TestSecondHandAt30Seconds(t *testing.T) {
 	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
@@ -26,3 +44,5 @@ func TestSecondHandAt30Seconds(t *testing.T) {
 		t.Errorf("Got %v, wanted %v", got, want)
 	}
 }
+
+*/
