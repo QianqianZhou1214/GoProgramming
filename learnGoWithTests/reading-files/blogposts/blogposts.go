ed080d12e20c48cd7de11daee0bc506adf1b9cd4
Qianqian Zhou <qqzh1214@gmail.com>
2025-10-24 22:27:16 +0200

    Handling errors

--- learnGoWithTests/reading-files/blogposts/blogposts.go
+++ learnGoWithTests/reading-files/blogposts/blogposts.go
@@ -8,14 +8,18 @@ type Post struct {
 }
 
 // NewPostsFromFS cannot use fstest.MapFS here as param because its a concrete impl not an abstract interface
-func NewPostsFromFS(fileSystem fs.FS) []Post {
-	dir, _ := fs.ReadDir(fileSystem, ".")
+func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
+	dir, err := fs.ReadDir(fileSystem, ".")
 	//fs.ReadDir reads a dir inside given fs.FS returning []DirEntry
+
+	if err != nil {
+		return nil, err
+	}
 	var posts []Post
 
 	//iterating over the entries, create a post for each one and return the slice
 	for range dir {
 		posts = append(posts, Post{})
 	}
-	return posts
+	return posts, nil
 }
