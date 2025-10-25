b113fa512e123a9d7580e0cccf24854651a5469a
Qianqian Zhou <qqzh1214@gmail.com>
2025-10-25 21:44:52 +0200

    refactor

--- learnGoWithTests/reading-files/blogposts/blogposts.go
+++ learnGoWithTests/reading-files/blogposts/blogposts.go
@@ -62,25 +62,12 @@ func newPost(postFile io.Reader) (Post, error) {
 		scanner.Scan()
 		return strings.TrimPrefix(scanner.Text(), tagName)
 	}
-	title := readMetaLine(titleSeparator)
-	description := readMetaLine(descriptionSeparator)
-	tags := strings.Split(readMetaLine(tagsSeparator), ", ")
-
-	scanner.Scan() // ignore a line
-
-	// scanner.Scan() returns a bool which indicates whether there is more data to scan
-	buf := bytes.Buffer{}
-	// for loop to scan through the end
-	for scanner.Scan() {
-		fmt.Fprintln(&buf, scanner.Text())
-	}
-	body := strings.TrimSuffix(buf.String(), "\n")
 
 	return Post{
-		Title:       title,
-		Description: description,
-		Tags:        tags,
-		Body:        body,
+		Title:       readMetaLine(titleSeparator),
+		Description: readMetaLine(descriptionSeparator),
+		Tags:        strings.Split(readMetaLine(tagsSeparator), ", "),
+		Body:        readBody(scanner),
 	}, nil
 	/*
 		return Post{
@@ -122,6 +109,19 @@ func newPost(postFile io.Reader) (Post, error) {
 		return post, nil
 
 	*/
+}
+
+func readBody(scanner *bufio.Scanner) string {
+	scanner.Scan() // ignore a line
+
+	// scanner.Scan() returns a bool which indicates whether there is more data to scan
+	buf := bytes.Buffer{}
+	// for loop to scan through the end
+	for scanner.Scan() {
+		fmt.Fprintln(&buf, scanner.Text())
+	}
+	return strings.TrimSuffix(buf.String(), "\n")
+
 }
 
 type StubFailingFS struct {
