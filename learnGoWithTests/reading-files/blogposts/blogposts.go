ddbea59acddb70c00f796490b55bf67f00284db3
Qianqian Zhou <qqzh1214@gmail.com>
2025-10-25 18:49:34 +0200

    refactor

--- learnGoWithTests/reading-files/blogposts/blogposts.go
+++ learnGoWithTests/reading-files/blogposts/blogposts.go
@@ -2,6 +2,7 @@ package blogposts
 
 import (
 	"errors"
+	"io"
 	"io/fs"
 )
 
@@ -24,6 +25,26 @@ func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
 		posts = append(posts, Post{})
 	}
 	return posts, nil
+}
+
+func getPost(fileSystem fs.FS, f fs.DirEntry) (Post, error) {
+	postFile, err := fileSystem.Open(f.Name())
+	// fs.FS gives us a way(.Open()) of opening file within it by name
+	if err != nil {
+		return Post{}, err
+	}
+	defer postFile.Close()
+	return newPost(postFile)
+}
+
+func newPost(postFile fs.File) (Post, error) {
+	postData, err := io.ReadAll(postFile)
+	if err != nil {
+		return Post{}, err
+	}
+
+	post := Post{Title: string(postData)[7:]}
+	return post, nil
 }
 
 type StubFailingFS struct {
