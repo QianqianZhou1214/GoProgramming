166ea6ce174ced2246252ee3618fc778238e54dd
Qianqian Zhou <qqzh1214@gmail.com>
2025-10-26 14:33:37 +0100

    test Stack of Ints and Strings

--- learnGoWithTests/generics/generics_test.go
+++ learnGoWithTests/generics/generics_test.go
@@ -14,3 +14,40 @@ func TestAssertFunctions(t *testing.T) {
 
 	// AssertEqual(t, 1, "1") // uncommented to see the error
 }
+
+func TestStack(t *testing.T) {
+	t.Run("integer stack", func(t *testing.T) {
+		myStackOfInts := new(StackOfInts)
+
+		AssertTrue(t, myStackOfInts.IsEmpty())
+
+		myStackOfInts.Push(123)
+		AssertTrue(t, myStackOfInts.IsEmpty())
+
+		myStackOfInts.Push(456)
+		value, _ := myStackOfInts.Pop()
+		AssertEqual(t, value, 456)
+		value, _ = myStackOfInts.Pop()
+		AssertEqual(t, value, 123)
+		AssertTrue(t, myStackOfInts.IsEmpty())
+	})
+
+	t.Run("string stack", func(t *testing.T) {
+		myStackOfStrings := new(StackOfStrings)
+
+		// check stack is empty
+		AssertTrue(t, myStackOfStrings.IsEmpty())
+
+		// add a thing, then check it's not empty
+		myStackOfStrings.Push("123")
+		AssertFalse(t, myStackOfStrings.IsEmpty())
+
+		// add another thing, pop it back again
+		myStackOfStrings.Push("456")
+		value, _ := myStackOfStrings.Pop()
+		AssertEqual(t, value, "456")
+		value, _ = myStackOfStrings.Pop()
+		AssertEqual(t, value, "123")
+		AssertTrue(t, myStackOfStrings.IsEmpty())
+	})
+}
